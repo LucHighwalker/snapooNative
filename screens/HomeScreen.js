4dcc3c79d81d841134ad68f765a06bef4c0bd472
LucHighwalker <luc.highwalker@gmail.com>
2019-04-30 14:09:15 -0700

    able to move image around

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,60 +1,98 @@
 import React from "react";
-import { Image, Animated, StyleSheet, Dimensions, View } from "react-native";
-import { WebBrowser } from "expo";
-
-import ImageResizeMode from "react-native/Libraries/Image/ImageResizeMode";
-
-import { MonoText } from "../components/StyledText";
+import {
+	Image,
+	Animated,
+	StyleSheet,
+	Dimensions,
+	View,
+	PanResponder
+} from "react-native";
 
 const requests = [
-  { name: "hi", image: require("../assets/images/1.png") },
-  { name: "hi", image: require("../assets/images/1.png") },
-  { name: "hi", image: require("../assets/images/1.png") },
-  { name: "hi", image: require("../assets/images/1.png") }
+	{ name: "hi", image: require("../assets/images/0.jpg") },
+	{ name: "hi", image: require("../assets/images/1.jpg") },
+	{ name: "hi", image: require("../assets/images/2.jpg") },
+	{ name: "hi", image: require("../assets/images/3.jpg") }
 ];
 
 const HEIGHT = Dimensions.get("window").height;
 const WIDTH = Dimensions.get("window").width;
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null
-  };
+	static navigationOptions = {
+		header: null
+	};
 
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View stlye={{ height: 60 }} />
-        <View stlye={{ flex: 1 }}>
-          <Animated.View
-            style={{
-              height: HEIGHT - 120,
-              width: WIDTH,
-              padding: 10
-            }}
-          >
-            <Image
-              resizeMode={ImageResizeMode.cover}
-              style={{
-                flex: 1,
-                height: null,
-                width: null,
-                resizeMode: "cover",
-                borderRadius: 20
-              }}
-              source={requests[0].image}
-            />
-          </Animated.View>
-        </View>
-        <View stlye={{ height: 60 }} />
-      </View>
-    );
-  }
+	constructor() {
+		super();
+
+		this.position = new Animated.ValueXY();
+		this.state = {
+			index: 0
+		};
+	}
+
+	componentWillMount() {
+		this.PanResponder = PanResponder.create({
+			onStartShouldSetPanResponder: (evt, gestureState) => true,
+			onPanResponderMove: (evt, gestureState) => {
+				this.position.setValue({
+					x: gestureState.dx,
+					y: gestureState.dy
+				});
+			},
+			onPanResponderRelease: (evt, gestureState) => {}
+		});
+	}
+
+	renderRequests = () => {
+		return requests
+			.map(({ name, image }, index) => (
+				<Animated.View
+					{...this.PanResponder.panHandlers}
+					key={index}
+					style={[
+            {
+              transform: this.position.getTranslateTransform()
+            },
+						{
+							position: "absolute",
+							height: HEIGHT - 120,
+							width: WIDTH,
+							padding: 10,
+							paddingTop: 40
+						}
+					]}
+				>
+					<Image
+						style={{
+							flex: 1,
+							height: null,
+							width: null,
+							resizeMode: "cover",
+							borderRadius: 20
+						}}
+						source={image}
+					/>
+				</Animated.View>
+			))
+			.reverse();
+	};
+
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				<View stlye={{ height: 60 }} />
+				<View stlye={{ flex: 1 }}>{this.renderRequests()}</View>
+				<View stlye={{ height: 60 }} />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#fff"
+	}
 });
